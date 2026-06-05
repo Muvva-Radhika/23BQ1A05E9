91bfc9ade84da9bbe383629f7c2ce0a8db75f727
Muvva-Radhika <radhikamuvva61@gmail.com>
2026-06-05 11:53:11 +0530

    Initial commi

---
+++ notification_app_fe/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PriorityInbox from './PriorityInbox';
+
+function App() {
+    return (
+        <div>
+            <PriorityInbox />
+        </div>
+    );
+}
+
+export default App;
